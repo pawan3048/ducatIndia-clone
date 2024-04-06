146a79bb1ea275326e85c385e30a08813141e4ac
Pawan Kumar <pawankumhar3048@gmail.com>
2024-04-06 11:16:45 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+
+// const slider = document.querySelector(".slider_container");
+// const orgSlide = document.querySelector(".org_slide");
+
+// function toggle() {
+//   const allCourses = document.querySelector(".all-courses");
+//   allCourses.style.display = "flex";
+// }
+// const viewAll = document.querySelector(".program_view-btn");
+// const show = document.querySelector("#ducat-programs");
+
+// viewAll.addEventListener("click", () => {
+//   show.style.height = "auto";
+// });
+// viewAll.addEventListener('dblclick',(e)=>{
+//   e.preventDefault()
+//   show.style.height = "450px";
+// })
+// const menuBtn = document.querySelector(".nav_menu");
+//   menuBtn.addEventListener('click',()=>{
+//   const links = document.querySelector(".nav_links");
+//   links.style.dipslay="block"
+
+// })
+
+// var slideCount = 0;
+
+// slider.forEach((img, index) => {
+//   img.style.bottom = `${index * 100}%`;
+//   img.style.transition=''
+// });
+
+// const slideImg = () => {
+//   slider.forEach((slide) => {
+//     slide.style.transform = `{translateX(-${slideCount * 100}%)}`;
+//     slide.style.transition = 'transform 5s 0s linear';
+//   });
+// };
+
+
+// let prevBtn = document.querySelector(".prevBtn");
+// let nextBtn = document.querySelector(".nextBtn");
+
+// prevBtn.addEventListener("click", () => {
+//   slideImg--;
+//   slideImg();
+// });
+// nextBtn.addEventListener("click", () => {
+//   slideImg++;
+//   slideImg();
+// });
+
+// orgSlide.forEach((img, index) => {
+//   img.style.left = `${index * 100}%`;
+// });
+// function leftBtn(){
+
+// }
+// function rightBtn(){}
+
+// const goPrev = () => {
+//   slideImg--;
+//   slideImg();
+// }
+// const goNext = () => {
+//   slideImg++;
+//   slideImg();
+
+// }
+
